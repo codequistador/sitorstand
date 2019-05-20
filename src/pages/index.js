41c540c23d7314c0308977f7ceceec8c7cc0ab22
Justin Daining <justin@justindaining.com>
2019-05-20 15:07:06 -0600

    adding a setting

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,9 @@ class Clock extends React.Component {
     super(props)
     this.state = {
       time: new Date().getMinutes(),
+      standDuration: "15",
     }
+    this.handleChange = this.handleChange.bind(this)
   }
   componentDidMount() {
     this.intervalID = setInterval(() => this.tick(), 30000)
@@ -20,9 +22,14 @@ class Clock extends React.Component {
       time: new Date().getMinutes(),
     })
   }
+  handleChange(event) {
+    this.setState({ standDuration: event.target.value })
+  }
   render() {
-    const sitOrStand = this.state.time > 44 ? "stand" : "sit"
-    const color = this.state.time > 44 ? "#763626" : "#336B87"
+    const sitOrStand =
+      this.state.time > 60 - this.state.standDuration ? "stand" : "sit"
+    const color =
+      this.state.time > 60 - this.state.standDuration ? "#763626" : "#336B87"
     return (
       <div
         css={css`
@@ -40,6 +47,36 @@ class Clock extends React.Component {
         `}
       >
         {sitOrStand}
+        <div
+          css={css`
+            position: absolute;
+            bottom: 20px;
+            right: 30px;
+            font-size: 18px;
+          `}
+        >
+          Stand for
+          <input
+            css={css`
+              -webkit-box-sizing: border-box;
+              box-sizing: border-box;
+              display: inline-block;
+              background-color: transparent;
+              color: white;
+              border: none;
+              border-bottom: 1px solid white;
+              outline: 0;
+              width: 30px;
+              font-size: 24px;
+              margin: 0 10px;
+            `}
+            type="text"
+            name="lastname"
+            value={this.state.standDuration}
+            onChange={this.handleChange}
+          />
+          minutes per hour
+        </div>
       </div>
     )
   }
